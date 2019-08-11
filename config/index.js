5a0cf2235ee83dcd8f91f6960e4b23bb7488ffe5
zhoutian <1971395525@qq.com>
2019-08-11 17:40:22 +0800

    version 0.0.2

--- config/index.js
+++ config/index.js
@@ -10,7 +10,15 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+        target: 'https://cxc.umdemo.cn', //是你要请求后台的域名和端口
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''     //路径重写，也可以写成 '^/api': 'http://f.apiplus.cn'
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
@@ -23,7 +31,7 @@ module.exports = {
     // Use Eslint Loader?
     // If true, your code will be linted during bundling and
     // linting errors and warnings will be shown in the console.
-    useEslint: true,
+    useEslint: false,
     // If true, eslint errors and warnings will also be shown in the error overlay
     // in the browser.
     showEslintErrorsInOverlay: false,
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,80 @@
 import Vue from 'vue';
-import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import VueRouter from 'vue-router';
+import Login from './../pages/Login/Login';
+import Register from './../pages/Register/Register';
+import DashBoard from './../pages/DasgBoard/DashBoard';
+import MainPage from './../components/MainPage';
+import CentralKitchenManage from '../components/CooperationManager/CentralKitchenManage';
+import OperationStoreManage from '../components/CooperationManager/OperationStoreManage';
+import OrderManage from '../components/OperationManager/OrderManage';
+import MemberManage from '../components/OperationManager/MemberManage';
+import CommodityStoreManage from '../components/OperationManager/CommodityStoreManage';
+import MenuManage from '../components/OperationManager/MenuManage';
+import StoreAdd from '../components/CooperationManager/AddStore';
+import GoodsAdd from '../components/OperationManager/AddGoods';
+import MenuAdd from '../components/OperationManager/AddMenu';
 
-Vue.use(Router);
+Vue.use(VueRouter);
 
-export default new Router({
+export default new VueRouter({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      component: Login,
+    },
+    {
+      path: '/register',
+      component: Register,
+    },
+    {
+      path: '/dashboard',
+      component: DashBoard,
+      children: [
+        {
+          path: 'main_page',
+          component: MainPage,
+        },
+        {
+          path: 'cooperationManger/centralKitchenManage',
+          component: CentralKitchenManage,
+        },
+        {
+          path: 'cooperationManger/centralKitchenManage/add',
+          component: StoreAdd,
+        },
+        {
+          path: 'cooperationManger/cooperationStoreManage',
+          component: OperationStoreManage,
+        },
+        {
+          path: 'operationManger/orderManage',
+          component: OrderManage,
+        },
+        {
+          path: 'operationManger/memberManage',
+          component: MemberManage,
+        },
+        {
+          path: 'operationManger/commodityStoreManage',
+          component: CommodityStoreManage,
+        },
+        {
+          path: 'operationManger/commodityStoreManage/add',
+          component: GoodsAdd,
+        },
+        {
+          path: 'operationManger/menuManage',
+          component: MenuManage,
+        },
+        {
+          path: 'operationManger/menuManage/add',
+          component: MenuAdd,
+        },
+      ],
     },
   ],
 });
