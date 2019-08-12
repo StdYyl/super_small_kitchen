35852fdd860a1a5b29fe485e5cbe3a08c382bf84
YYL <528243772@qq.com>
2019-08-12 09:37:48 +0800

    version 0.0.3

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <router-view/>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'App',
-};
+<script>fun
+    export default {
+        name: 'App',
+    };
 </script>
 
 <style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  body, div, dl, dt, dd, ul, ol, li, h1, h2,h3,form,fieldset,input,textarea,p,blockquote,th,td { margin:0; padding:0; }
+  ol,ul{
+    list-style: none;
+  }
+  /*左浮动*/
+  .leftfloat{float: left;}
+  /*右浮动*/
+  .rightfloat{float: right;}
+  /*链接字体颜色*/
+  a{color: #646464;text-decoration:none;}
+  /*清楚浮动*/
+  .clearfix:after{content:"";display: block;clear: both;}
+  .clearfix{zoom: 1;}
+  .none{
+    display: none;
+  }
+  input,body,img{border: none;}
+
+
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,41 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import Axios from 'axios';
-import AMap from 'vue-amap';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
 import App from './App';
 import router from './router';
-import './icons/index';
-import store from './store/index';
+
+import  ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import store from './store'
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false;
-Vue.use(ElementUI);
-Vue.prototype.axios = Axios;
-Vue.use(AMap);
-
-AMap.initAMapApiLoader({
-  // 高德的key
-  key: 'c23a73bb2c062de16b9158396b2817de&plugin=AMap.Geocoder',
-  // 插件集合
-  plugin: [
-    'AMap.Autocomplete',
-    'AMap.PlaceSearch',
-    'AMap.Scale',
-    'AMap.OverView',
-    'AMap.ToolBar',
-    'AMap.MapType',
-    'AMap.PolyEditor',
-    'AMap.CircleEditor',
-    'AMap.Geolocation',
-    'AMap.CitySearch',
-  ],
-  v: '1.4.4',
-});
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App),
+  components: { App },
+  template: '<App/>',
 });
